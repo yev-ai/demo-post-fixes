b8d486ce9fe6ecddeb2b46cf1d5b8cb2604f3621
NotYevvie <yevgen@yev.ai>
2025-03-23 19:05:27 -0500

    use buildkit and add postgres service

--- .cicd/build-docker.sh
+++ .cicd/build-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Note - this is meant to be ran from package.json
-docker build -f ./.cicd/Dockerfile -t nextjs-15-demo .
+DOCKER_BUILDKIT=1 docker build -f ./.cicd/Dockerfile -t nextjs-15-demo .
 
 echo "Docker Image Layers:"
 docker history nextjs-15-demo
